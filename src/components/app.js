e0f8e451daf312be4be56fcabfdc7a786f9ffa53
dimrakitin <dimrakitin@yandex.ru>
2017-09-08 13:57:36 +0300

    Add new component

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react'
+import EditNote from './edit-note/edit-note'
 
 import './app.sass'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div>
         <h1 id='app-head'>Notes</h1>
+        <EditNote/>
       </div>
     );
   }
 }
-
-export default App
---
+++ src/components/edit-note/edit-note.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+import './edit-note.sass'
+
+export default class EditNote extends Component {
+  render() {
+    return (
+      <div id='edit-note'>
+        <h1>EditNote</h1>
+      </div>
+    );
+  }
+}
